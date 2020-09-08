2c24c9300a690f4c563fb84a6cefdaf5d979725a
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-09-08 12:38:18 -0700

    13: Lambda created which will delete a user theme from the DB when provided with userId and themeId on pathParameters object

---
+++ deleteUserTheme/index.js
@@ -0,0 +1,45 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "us-west-2" });
+
+exports.handler = async (event, context) => {
+  const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+  const documentClient = new AWS.DynamoDB.DocumentClient({
+    region: "us-west-2",
+  });
+
+  let responseBody = "";
+  let statusCode = 0;
+
+  const { userId, themeId } = event.pathParameters;
+
+  const params = {
+    TableName: "Themes",
+    KeyConditionExpression: "userId = :userId, themeId = :themeId",
+    ExpressionAttributeValues: {
+      ":userId": userId,
+      ":themeId": themeId,
+    },
+  };
+
+  try {
+    const data = await documentClient.delete(params).promise();
+    responseBody = JSON.stringify(data);
+    statusCode = 200;
+  } catch (err) {
+    responseBody = `Unable to delete theme data`;
+    statusCode = 403;
+  }
+
+  const response = {
+    statusCode,
+    body: responseBody,
+    headers: {
+      "Content-Type": "application/json",
+      "access-control-allow-origin": "*",
+    },
+  };
+
+  return response;
+};
